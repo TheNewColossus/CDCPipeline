c64b58e05a9fa37c83bdb3ab520642adca0daef6
Aayushh Dutta <aayushhdutta2001@gmail.com>
2025-02-07 17:23:25 +0530

    writing variables to env file

--- start.sh
+++ start.sh
@@ -2,9 +2,11 @@
 
 export WORK_DIR=$(pwd)
 
-if grep -fxq "WORKDIR=$WORK_DIR" ${WORK_DIR}/.env
-  then 
-    echo
+# Conditional statement for setting workdir variable when the program boots for the first time
+if grep -Fxq "WORKDIR=$WORK_DIR" ${WORK_DIR}/.env
+then
+    echo $WORK_DIR
 else
-  sed -i "2iWORKDIR=$WORK_DIR" ${WORK_DIR}/.env
+    sed -i "2iWORKDIR=$WORK_DIR" ${WORK_DIR}/.env
+    echo $WORK_DIR
 fi
