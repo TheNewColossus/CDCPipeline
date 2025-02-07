05bcf26ff230bd8ad7d7bf259c67408f899894e9
Aayushh Dutta <aayushhdutta2001@gmail.com>
2025-02-07 17:48:57 +0530

    starts the compose cluster

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ export WORK_DIR=$(pwd)
 # Conditional statement for setting workdir variable when the program boots for the first time
 if grep -Fxq "WORKDIR=$WORK_DIR" ${WORK_DIR}/.env
 then
-    echo $WORK_DIR
+    echo 
 else
     sed -i "2iWORKDIR=$WORK_DIR" ${WORK_DIR}/.env
     sudo docker compose up -d
