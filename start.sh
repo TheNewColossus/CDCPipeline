9ddd7afe6f180026f7e76772f200f596a68b9fff
Aayushh Dutta <aayushhdutta2001@gmail.com>
2025-02-09 15:05:54 +0530

    mysql directory will be made at the first time project is launched

--- start.sh
+++ start.sh
@@ -8,7 +8,8 @@ then
     echo 
 else
     sed -i "2iWORKDIR=$WORK_DIR" ${WORK_DIR}/.env
-    sudo docker buildx build -t confluentinc/cp-kafka-connected:7.8.0 .
+    sudo docker buildx build -t confluentinc/cp-mysql-server-connect:7.3.2 .
+    sudo mkdir ${WORK_DIR}/mysql
 fi
 
 sudo docker compose up -d
