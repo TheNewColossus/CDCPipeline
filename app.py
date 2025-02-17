f0bf3112e7f90cc26c69c2e3d2a1adbff81d4041
Aayushh Dutta <aayushhdutta2001@gmail.com>
2025-02-17 17:34:52 +0530

    this python file contains the streaming code

---
+++ app.py
@@ -0,0 +1,25 @@
+# *-* coding: utf-8 *-*
+
+#Sparksession to create a spark instance
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.master("spark://spark-master:7077").appName("cdc_pipeline").getOrCreate()
+
+#Dataframe to hold the messages from the stream
+df = spark \
+  .readStream \
+  .format("kafka") \
+  .option("kafka.bootstrap.servers", "broker:29092") \
+  .option("subscribe", "change_captured.public.students") \
+  .load()
+df = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+
+#Writing the stream in order to read it, it is a bit silly but a procedure that needs to be followed
+query = df \
+    .writeStream \
+    .outputMode("append") \
+    .format("console") \
+    .queryName("blah") \
+    .start()
+
+#Hoping for the best
+query.awaitTermination()
